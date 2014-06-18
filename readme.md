08edae4e9feb98a524ceec8928fe56ec4f0ee8e1
Matthew McKenna <mpmckenna8@me.com>
2014-06-18 14:46:06 -0700

    hopefully making a github pages site

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Find Maptime Stuff using the Twitter API
+
+The files here w/ a .js extension are scripts for which there's a little bit of setup required to make them work. First off you'll need Node.js w/ NPM installed and install npm twit in the directory with the scripts.
+
+Also needed are the appropriate API keys from Twitter.  To Attain these visit https://dev.twitter.com/ and login.  You'll need to say you're making an App so do that and also generate an access token. If you just plan on querying the API you only need to give it read permissions, but if you also want to post stuff to twitter using the API give it write permissions as well.  
+
+# What the different files do
+
+twQuery.js
+  
---
+++ twd3.js
@@ -0,0 +1,95 @@
+// just use this to make table http://jsfiddle.net/7WQjr/
+
+var tweTab = d3.select('body').append('div').attr('id', 'container');
+
+
+var mapCle = d3.json('mapTwit/maptimes.json', function(err,data){
+  if(err){console.log(err)}
+  else{
+     console.log(data)
+
+function tabulate(data, columns) {
+    var table = d3.select("#container").append("table"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
+
+    // append the header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+            .text(function(column,i) {
+          //    console.log(column)
+              return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr")
+        .attr('class',function(d,i){
+        //  console.log(d);
+          var bicol = (i%2);
+          if(bicol ===0){
+            return 'eve cell'
+          }
+          else{
+            return 'odd cell'
+          }
+        })
+;
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll("td")
+        .data(function(row,i) {
+        //  console.log(i);
+            return columns.map(function(column) {
+                return {column: column, value: row[column]};
+            });
+        })
+        .enter()
+        .append("td")
+            .html(function(d,i) {
+              //console.log(i);
+//console.log(typeof(d.value))
+              if(typeof(d.value)==='object'){
+                return '<p>' + d.value.name +'</p><p>@' +d.value.screen_name +'</p>'
+              }
+              else{return d.value}
+              ; })
+
+            cells.attr('class','god');
+
+            d3.selectAll('rows')
+            .attr('class', function(d,i){
+              console.log(i);
+              return 'rower'
+            })
+
+              ;
+
+    return table;
+}
+
+// create some people
+
+// render the table
+var peopleTable = tabulate(data, ["user", "text"]);
+
+// uppercase the column headers
+peopleTable.selectAll("thead th")
+    .text(function(column) {
+        return column.charAt(0).toUpperCase() + column.substr(1);
+    });
+
+// sort by age
+
+
+//peopleTable.selectAll('tr').attr('class','rower')
+    for (var o in data){
+    //  tweTab.append('tr').append('td').
+    // console.log(data[o].user.name)
+    };
+  }
+})
